7cc56f5dd573d2e81995f982d1ee58f86a76807a
Angel Brambila <malthael.91@gmail.com>
2021-02-26 10:55:34 -0600

    Casting, Iteration Statements and Exceptions projects added

---
+++ 1P/LS03/HandlingExceptions/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using static System.Console;
+namespace HandlingExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region try Catch
+            Write("Before Parsing");
+            Write("What's your age?");
+            string input = ReadLine();
+                try
+                {
+                    int age = int.Parse(input);
+                }
+                catch(FormatException)
+                {
+                    WriteLine("The age you entered is not a valid number format");
+                }
+                catch(OverflowException)
+                {
+                    WriteLine("You age is a valid number but it is either too big or small");
+                }
+                catch (Exception ex)
+                {
+                    WriteLine($"{ex.GetType()} says {ex.Message}");
+                }
+                WriteLine("After Parsing");
+            #endregion
+
+            #region Checking overflow
+                // checked
+                try
+                {
+                checked
+                {
+                int x = int.MaxValue - 1;
+                WriteLine($"Initial Value {x}");
+                x++;
+                WriteLine($"After incrementing {x}");
+                x++;
+                WriteLine($"After incrementing {x}");
+                x++;
+                WriteLine($"After incrementing {x}");
+                }
+                }
+                catch (OverflowException)
+                {
+                    WriteLine("Something went wrong ...");
+                }
+            #endregion
+        }
+    }
+}
