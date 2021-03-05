7b9a42855cdb935a53e6b3d760f41fdc7ca025d2
Angel Brambila <malthael.91@gmail.com>
2021-03-05 10:09:28 -0600

    Added Debugging and Unit Testing Projects

---
+++ 1P/LS04/DebuggingAndLogging/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+using System.Diagnostics;
+using System.IO;
+
+namespace DebuggingAndLogging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Debug -> Debug run
+            // Trace -> Release run
+            Trace.Listeners.Add(new TextWriterTraceListener(
+                File.CreateText("log.txt")
+            ));
+            Trace.AutoFlush = true;
+            Debug.WriteLine("Debug says, IM WATCHING YOU!!! ");
+            Trace.WriteLine("Trace says, IVAN ET NIOJ");
+        }
+    }
+}
