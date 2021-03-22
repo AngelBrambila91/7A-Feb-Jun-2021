2bdea32cee2c832cfef05c9cf09f95d41d0565b5
Angel Brambila <malthael.91@gmail.com>
2021-03-22 10:39:59 -0600

    Simplified Methods and Delegates

---
+++ 1P/LS05/GeneralLibrary/Person.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace GeneralLibrary
+{
+    public class Person : object
+    {
+        // Member Variables
+        /*
+        Fileds: 
+            - Constant : Data can never change
+            - Read - Only : Data can never change after class is instantiated, BUT i can change data from constructor
+            - Event : 
+        
+        Methods :
+            - Constructor : when the new keyword is called
+            - Property : 
+            - Indexer : 
+            - Operator : 
+        */
+        // Fields
+        public string Name;
+        public DateTime DateOfBirth;
+        public WondersOfTheWorld FavoriteWonder;
+        public List<Person> Children = new List<Person>();
+        public const string Species = "Homo Sapiens";
+        public readonly string HomePlanet = "Earth";
+        public readonly DateTime Instantiated;
+        /*
+        private : Only inside the class is accesible
+        internal : Only accesible inside the same assembly
+        protected : Only accesible from any type that inherits
+        public : member is accesible everywhere
+        internal protected:
+        private protected:
+        */
+        // Contructors
+        public Person()
+        {
+            Name = "Unkown";
+            Instantiated = DateTime.Now;
+        }
+
+        public Person(string initialName, string homePlanet)
+        {
+            Name = initialName;
+            HomePlanet = homePlanet;
+            Instantiated = DateTime.Now;
+        }
+
+        #region Methods
+        public void WriteToConsole()
+        {
+            WriteLine($"{Name}, was born on  a {DateOfBirth:dddd}.");
+        }
+
+        // static method to multiply
+        public static Person Procreate(Person p1, Person p2)
+        {
+            var baby = new Person
+            {
+                Name = $"Baby of {p1.Name} and {p2.Name}"
+            };
+            p1.Children.Add(baby);
+            p2.Children.Add(baby);
+            return baby;
+        }
+
+        // instance method to multiply
+        public Person ProceateWith(Person partner)
+        {
+            return Procreate(this, partner);
+        }
+
+        // overload * operator to use as procreate
+        public static Person operator * (Person p1, Person p2)
+        {
+            return Person.Procreate(p1, p2);
+        }
+        #endregion
+
+        #region Local Functions
+            // Initial Method
+            public static int Factorial(int number)
+            {
+                if( number < 0)
+                {
+                    throw new ArgumentException($"{nameof(number)} cannot be less than zero.");
+                }
+                return localFactorial(number);
+                int localFactorial (int localNumber) // local function
+                {
+                    if(localNumber < 1) return 1;
+                    return localNumber * localFactorial(localNumber -  1);
+                }
+            }
+        #endregion
+
+        #region Delegates
+        public int MethodICall(string input)
+        {
+            return input.Length;
+        }
+
+        // Event Handler
+        public event EventHandler Shout;
+        // field
+        public int AngerLevel;
+        // method
+        public void Poke()
+        {
+            AngerLevel++;
+            if(AngerLevel >= 3)
+            {
+                if(Shout != null)
+                {
+                    //call the delegate
+                    Shout(this, EventArgs.Empty);
+                }
+            }
+        }
+        #endregion
+
+
+    }
+}
---
+++ 1P/LS05/PeopleApp/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using GeneralLibrary;
+using static System.Console;
+
+namespace PeopleApp
+{
+    class Program
+    {
+        delegate int DelegateWithMatchSignature (string s);
+        static void Main(string[] args)
+        {
+            #region POO Stuff
+                
+            var baltazar = new Person();
+            baltazar.Name = "Baltazar";
+            WriteLine(baltazar.ToString());
+
+            var leo = new Person();
+            leo.Name = "Leonardo";
+            leo.DateOfBirth = new DateTime(1990, 12, 22);
+            WriteLine($"{leo.Name} was born in {leo.DateOfBirth: d MMMM yyyy}");
+
+            var layla = new Person
+            {
+                Name = "Layla",
+                DateOfBirth = new DateTime(1980, 3, 7),
+                FavoriteWonder = WondersOfTheWorld.ColossusOfRhodes
+            };
+            WriteLine($"{layla.Name} was born in {layla.DateOfBirth: d MMMM yyyy}");
+            baltazar.FavoriteWonder = WondersOfTheWorld.HangingGardensOfBabylon;
+            
+            baltazar.Children.Add(new Person
+            {Name = "Alfred"});
+            baltazar.Children.Add(new Person
+            {Name = "Sakura Kinomot Desu"});
+
+            WriteLine($"{baltazar.Name} has {baltazar.Children.Count} children :");
+            for (int child = 0; child < baltazar.Children.Count; child++)
+            {
+                WriteLine($"{baltazar.Children[child].Name}");
+            }
+
+            BankAccount.InterestRate = 0.012M;
+            var leoAccount = new BankAccount();
+            leoAccount.AccountName = "Mister Leo";
+            leoAccount.Balance = 3400;
+            WriteLine($"{leoAccount.AccountName} earned {leoAccount.Balance * BankAccount.InterestRate:C} interest");
+
+            var laylaAccount = new BankAccount();
+            laylaAccount.AccountName = "Miss Layla";
+            laylaAccount.Balance = 3400;
+            WriteLine($"{laylaAccount.AccountName} earned {laylaAccount.Balance * BankAccount.InterestRate:C} interest");
+
+            var blankPerson = new Person();
+            WriteLine($"{blankPerson.Name} of {blankPerson.HomePlanet} was created {blankPerson.Instantiated}");
+
+            var martian = new Person("Marvin", "Mars");
+            WriteLine($"{martian.Name} of {martian.HomePlanet} was created {martian.Instantiated}");
+            #endregion
+
+            #region Procreate
+                var ana = new Person { Name = "Ana" };
+                var tiberio = new Person { Name = "Tiberio" };
+                var vicente = new Person { Name = "Vicente" };
+                // call instance method
+                var baby1 = ana.ProceateWith(tiberio);
+                baby1.Name = "NoPalindromo";
+                // static call
+                var baby2 = Person.Procreate(vicente, ana);
+
+                // ussing overload operator
+                var baby3  = ana * tiberio;
+                WriteLine($"{ana.Name} has {ana.Children.Count} children");
+                WriteLine($"{tiberio.Name} has {tiberio.Children.Count} children");
+                WriteLine($"{vicente.Name} has {vicente.Children.Count} children");
+                WriteLine($"{ana.Name}'s first child is named \"{ana.Children[0].Name}\".");
+
+
+            #endregion
+
+            #region TestLocal Functions
+                WriteLine($"5! is {Person.Factorial(5)}");
+            #endregion
+            var p1 = new Person();
+            int answer = p1.MethodICall("Something");
+            WriteLine(answer);
+            // create delegate instance
+            var d = new DelegateWithMatchSignature(p1.MethodICall);
+            // call delegate
+            int asnwer2 = d("Amibas");
+            WriteLine(asnwer2);
+
+            var dali = new Person();
+            dali.Name = "Dali";
+            dali.Shout += Dali_Shout;
+            dali.Shout += Oscar_Shout;
+            dali.Shout += Tach_Shout;
+            dali.Poke();
+            dali.Poke();
+            dali.Poke();
+            dali.Poke();
+            dali.Poke();
+
+        }
+        #region Using Delegates
+        private static void Dali_Shout(object sender, EventArgs e)
+        {
+            Person p = (Person)sender;
+            WriteLine($"{p.Name} is this angry : {p.AngerLevel}");
+        }
+
+        private static void Oscar_Shout(object sender, EventArgs e)
+        {
+            
+        }
+
+        private static void Tach_Shout(object sender, EventArgs e)
+        {
+            WriteLine("Hello Govnr");
+        }
+        #endregion
+    }
+}
